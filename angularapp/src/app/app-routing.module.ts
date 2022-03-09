bed3bb8046b2a88276e1e11d7aab3b523369c9da
SivaIlakkiya <sivaila314@gmail.com>
2022-03-09 09:26:38 +0000

    final commit

---
+++ angularapp/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/signup',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    data: { showHeader: false, showFooter: false }
+    // outlet: 'popup'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { showHeader: false, showFooter: false }
+    // outlet: 'popup'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  title ='d2h'
+ }
