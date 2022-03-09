81a35dd10820a31ddeecf739971dfeae1dbc328c
SivaIlakkiya <sivaila314@gmail.com>
2022-03-09 09:41:56 +0000

    demo message

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
