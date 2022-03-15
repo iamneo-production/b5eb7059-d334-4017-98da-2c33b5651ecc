c65ab11be2677c33d710552d4a7cbb47ac2718f7
Arnab-2000 <arnabmukherjee486@gmail.com>
2022-03-15 21:05:18 +0000

    Hi

--- angularapp/src/app/app.component.spec.ts
+++ angularapp/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('exam-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularapp app is running!');
   });
 });
--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -8,9 +8,6 @@ public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
-		System.out.println("Hello");
-
 	}
 
-	
 }
